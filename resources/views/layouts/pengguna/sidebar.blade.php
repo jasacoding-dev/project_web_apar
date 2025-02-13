78ddf459f7e4b3e3baf79684203501ce65505271
vikararya <148735840+vikararya@users.noreply.github.com>
2025-02-13 20:18:50 +0700

    sidebar

--- resources/views/layouts/pengguna/sidebar.blade.php
+++ resources/views/layouts/pengguna/sidebar.blade.php
@@ -19,22 +19,32 @@
       </div>
       <nav class="mt-4 flex-1 px-4 space-y-4 font-bold">
         <ul>
-          <li>
-            <a href="/admin/dashboard" class="flex items-center px-4 py-2 hover:bg-blue-700 rounded text-white">
-              <img src="{{ asset('storage/ke 6.png') }}" alt="Icon" class="w-5 h-5 mr-2"> Beranda
-            </a>
-          </li>
+        <li>
+    <a href="/admin/dashboard" class=" flex items-center px-4 py-2 rounded text-white hover:bg-blue-700">
+        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" class="w-5 h-5 mr-2">
+            <path d="M4 21V9L12 3L20 9V21H14V14H10V21H4Z" fill="white"/>
+        </svg>
+        Beranda
+    </a>
+</li>
+
           <li class="mt-4">
-            <a href="/admin/pengguna" class="bg-white flex items-center px-4 text-[#223E88] py-2 font-bold rounded">
-              <img src="{{ asset('storage/pengguna.png') }}" alt="Icon" class="w-5 h-5 mr-2">
-              Pengguna
-            </a>
-          </li>
-          <li class="mt-4">
+    <a href="/admin/pengguna" class="bg-white flex items-center px-4 text-[#223E88] py-2 font-bold rounded">
+        <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg" class="mr-2">
+            <path d="M1.66663 3.3275C1.66815 3.10865 1.75571 2.89918 1.91039 2.74435C2.06506 2.58951 2.27444 2.50175 2.49329 2.5H17.5066C17.9633 2.5 18.3333 2.87083 18.3333 3.3275V16.6725C18.3318 16.8914 18.2442 17.1008 18.0895 17.2557C17.9349 17.4105 17.7255 17.4983 17.5066 17.5H2.49329C2.27397 17.4998 2.06371 17.4125 1.9087 17.2573C1.75369 17.1022 1.66663 16.8918 1.66663 16.6725V3.3275ZM4.99996 12.5V14.1667H15V12.5H4.99996ZM4.99996 5.83333V10.8333H9.99996V5.83333H4.99996ZM11.6666 5.83333V7.5H15V5.83333H11.6666ZM11.6666 9.16667V10.8333H15V9.16667H11.6666ZM6.66663 7.5H8.33329V9.16667H6.66663V7.5Z" fill="#223E88"/>
+        </svg>
+        Pengguna
+    </a>
+</li>
+
+<li class="mt-4">
     <button class="flex items-center justify-between w-full px-4 py-2 hover:bg-blue-700 rounded" 
         onclick="toggleDropdown('componentsDropdown', 'dropdownIcon')">
         <div class="flex items-center">
-        <img src="{{ asset('storage/ke3.png') }}" alt="Icon" class="w-5 h-5 mr-2">
+            <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg" class="w-5 h-5 mr-2">
+                <path d="M16.6665 1.66663H3.33317C2.49984 1.66663 1.6665 2.41663 1.6665 3.33329V5.84163C1.6665 6.44163 2.02484 6.95829 2.49984 7.24996V16.6666C2.49984 17.5833 3.4165 18.3333 4.1665 18.3333H15.8332C16.5832 18.3333 17.4998 17.5833 17.4998 16.6666V7.24996C17.9748 6.95829 18.3332 6.44163 18.3332 5.84163V3.33329C18.3332 2.41663 17.4998 1.66663 16.6665 1.66663ZM12.4998 11.6666H7.49984V9.99996H12.4998V11.6666ZM16.6665 5.83329H3.33317V3.33329L16.6665 3.31663V5.83329Z"
+                 fill="white"/>
+            </svg>
             Inventaris
         </div>
         <svg id="dropdownIcon" class="w-4 h-4 transition-transform transform duration-300" fill="currentColor" 
@@ -64,23 +74,28 @@
     }
 </script>
 
-                    <li class="mt-4">
-                        <a href="/admin/daftarlokasi" class="flex items-center px-4 py-2 hover:bg-blue-700 rounded">
-                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-5 h-5 mr-2">
-                                <path fill-rule="evenodd" d="m11.54 22.351.07.04.028.016a.76.76 0 0 0 .723 0l.028-.015.071-.041a16.975 16.975 0 0 0 1.144-.742 19.58 19.58 0 0 0 2.683-2.282c1.944-1.99 3.963-4.98 3.963-8.827a8.25 8.25 0 0 0-16.5 0c0 3.846 2.02 6.837 3.963 8.827a19.58 19.58 0 0 0 2.682 2.282 16.975 16.975 0 0 0 1.145.742ZM12 13.5a3 3 0 1 0 0-6 3 3 0 0 0 0 6Z" clip-rule="evenodd" />
-                            </svg>
-                            Lokasi
-                        </a>
-                    </li>
+                   
+<li class="mt-4">
+    <a href="/admin/daftarlokasi" class="flex items-center px-4 py-2 hover:bg-blue-700 rounded">
+        <svg width="26" height="26" viewBox="0 0 26 26" fill="none" xmlns="http://www.w3.org/2000/svg" class="w-5 h-5 mr-2">
+            <path d="M12.9998 12.4584C12.2815 12.4584 11.5927 12.1731 11.0848 11.6652C10.5768 11.1573 10.2915 10.4684 10.2915 9.75008C10.2915 9.03179 10.5768 8.34291 11.0848 7.835C11.5927 7.32709 12.2815 7.04175 12.9998 7.04175C13.7181 7.04175 14.407 7.32709 14.9149 7.835C15.4228 8.34291 15.7082 9.03179 15.7082 9.75008C15.7082 10.1057 15.6381 10.4579 15.502 10.7865C15.3659 11.1151 15.1664 11.4137 14.9149 11.6652C14.6634 11.9167 14.3649 12.1161 14.0363 12.2523C13.7077 12.3884 13.3555 12.4584 12.9998 12.4584ZM12.9998 2.16675C10.9886 2.16675 9.05976 2.9657 7.63761 4.38786C6.21546 5.81001 5.4165 7.73886 5.4165 9.75008C5.4165 15.4376 12.9998 23.8334 12.9998 23.8334C12.9998 23.8334 20.5832 15.4376 20.5832 9.75008C20.5832 7.73886 19.7842 5.81001 18.3621 4.38786C16.9399 2.9657 15.0111 2.16675 12.9998 2.16675Z" 
+            fill="white"/>
+        </svg>
+        Lokasi
+    </a>
+</li>
 
-                    
-                    <li class="mt-4">
-                        <a href="/admin/daftarbarcode" class=" flex items-center px-4 text-white py-2 hover:bg-blue-700 font-bold rounded">
-                        <img src="{{ asset('storage/ke5.png') }}" alt="Icon" class="w-5 h-5 mr-2" style="filter: hue-rotate(210deg);">
-                        Barcode
-                        </a>
-                    </li>
-
+<li class="mt-4">
+    <a href="/admin/daftarbarcode" class=" text-white flex items-center px-4 py-2 font-bold rounded hover:bg-blue-700">
+        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" class="w-5 h-5 mr-2">
+            <path d="M14 3V7C14 7.26522 14.1054 7.51957 14.2929 7.70711C14.4804 7.89464 14.7348 8 15 8H19" fill="yellow"/>
+            <path d="M14 3V7C14 7.26522 14.1054 7.51957 14.2929 7.70711C14.4804 7.89464 14.7348 8 15 8H19" stroke="#223E88" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+            <path d="M17 21H7C6.46957 21 5.96086 20.7893 5.58579 20.4142C5.21071 20.0391 5 19.5304 5 19V5C5 4.46957 5.21071 3.96086 5.58579 3.58579C5.96086 3.21071 6.46957 3 7 3H14L19 8V19C19 19.5304 18.7893 20.0391 18.4142 20.4142C18.0391 20.7893 17.5304 21 17 21Z" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+            <path d="M12 13V16M8 13H9V16H8V13ZM15 13H16V16H15V13Z" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+        </svg>
+        Barcode
+    </a>
+</li>
                  <!-- Log Out -->
                  <li class="absolute bottom-4 left-1/2 transform -translate-x-1/2 w-[98%] px-4">
     <button onclick="toggleModal()" class="bg-red-500 w-full text-white flex items-center px-4 py-2 rounded ">
