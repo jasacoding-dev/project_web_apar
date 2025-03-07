5b4a8c843c07ac6c75dc3907a0ffd8429e14a66f
vikararya <148735840+vikararya@users.noreply.github.com>
2025-03-07 18:11:03 +0700

    qqq

--- resources/views/admin/profile/profil.blade.php
+++ resources/views/admin/profile/profil.blade.php
@@ -19,14 +19,25 @@
             <div class="bg-white shadow-md rounded-b-lg p-2">
                 <h2 class="text-lg font-bold mb-4">Detail Profil</h2>
 
-                <!-- Foto Profil -->
-                <div class="flex flex-col md:flex-row items-center pb-2 mb-6 border border-black rounded-lg p-4 -mt-2">
-                    <img src="{{ asset('img/person-dummy.jpg') }}" alt="Profile Picture" class="w-16 h-16 rounded-full border mb-3 md:mb-0 md:mr-4">
-                    <div class="text-center md:text-left">
-                        <p class="text-sm font-bold">{{ $user->name }}</p>
-                        <p class="text-sm text-gray-600">{{ $role }}</p>
-                    </div>
-                </div>
+               <!-- Foto Profil -->
+<div class="relative flex flex-col md:flex-row items-center pb-2 mb-6 border border-black rounded-lg p-4 -mt-2">
+    <div class="relative">
+        <!-- Foto Profil -->
+        <img src="{{ asset('img/person-dummy.jpg') }}" alt="Profile Picture" class="w-16 h-16 rounded-full border mb-3 md:mb-0 md:mr-4">
+        
+        <!-- Tombol Edit -->
+        <button class="absolute bottom-0 right-0 transform translate-x-1/4 translate-y-1/4 bg-white border border-gray-300 p-1.5 rounded-full shadow-md mr-5">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-3 h-3 text-gray-600">
+  <path stroke-linecap="round" stroke-linejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" />
+</svg>
+
+        </button>
+    </div>
+    <div class="text-center md:text-left">
+        <p class="text-sm font-bold">{{ $user->name }}</p>
+        <p class="text-sm text-gray-600">{{ $role }}</p>
+    </div>
+</div>
 
                 <!-- Informasi Data Diri -->
                 <div class="bg-white border border-black p-4 rounded-lg shadow-inner relative -mt-4">
